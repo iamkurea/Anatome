0413a31fe0272e2f0ffa8671ebabf8e01c0546f7
iamkurea <iamkurea0913@gmail.com>
2026-03-09 13:44:53 +0800

    Update ribcage-test1-three.js

--- src/ribcage-test1-three.js
+++ src/ribcage-test1-three.js
@@ -46,7 +46,7 @@ function init() {
     sceneFG.add(fillLight.clone());
 
     const loader = new GLTFLoader();
-    loader.load('./public/openribcage.glb', (gltf) => {
+    loader.load('./openribcage.glb', (gltf) => {
         ribcageModel = gltf.scene;
         animationsData = gltf.animations;
 
